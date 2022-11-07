13a53cc1dc9ddb26c88f036051cf5ee9a2f26445
elfandor2 <achmd.yoga@gmail.com>
2022-11-07 17:39:12 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores impedit distinctio debitis incidunt obcaecati architecto sequi aliquam quae non, fuga tempora suscipit sit eius ipsa minima laudantium qui saepe? Maxime!</p>
 </body>
 </html>
